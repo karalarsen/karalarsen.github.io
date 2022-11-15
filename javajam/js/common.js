d5de0d471e9b7fbe0a1742d297eb8fc9d9e5a34f
karalarsen <A02330967@aggies.usu.edu>
2022-11-14 22:15:40 -0700

    JavaScript updates to JavaJam

---
+++ javajam/js/common.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $("#mugClub").mouseover(function() {
+        alert("JavaJam Mug Club Members get a 10% discount on each cup of coffee!")
+    });
+});
---
+++ javajam/js/verifysenior.js
@@ -0,0 +1,4 @@
+userAge = prompt("Enter Your Age: ")
+
+if (userAge >= 65) {document.getElementById("verify").innerHTML = "Free Friday Coffee Night for Seniors!"}
+else {document.getElementById("verify").innerHTML = "Enjoy Music and Make Memories!"};
