4f522818131f88d0195d47de168885c736932074
karalarsen <A02330967@aggies.usu.edu>
2022-11-28 15:38:45 -0700

    Few changes

--- project/js/dateupdate.js
+++ project/js/dateupdate.js
@@ -1,1 +1,1 @@
-document.getElementById("dateupdate").innerHTML = "This page was last modified on: " + document.lastModified;
+document.getElementById("dateupdate").innerHTML = "Last Updated: " + document.lastModified;
