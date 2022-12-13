3702f97b8b7ab783840747464acf0e88ce10211a
karalarsen <A02330967@aggies.usu.edu>
2022-12-12 20:40:07 -0700

    Color Contrast

--- project/js/closed.js
+++ project/js/closed.js
@@ -4,13 +4,13 @@ var month = date.getMonth() + 1;
 
 if (month < 6 || month > 8) {
     document.getElementById("closed").innerHTML = "We are currently closed for the season, come back between June 5th and August 20th!";
-    document.getElementById("closed").style.color='red';
+    document.getElementById("closed").style.color= 'maroon';
 } else if (month === 6 && day < 5) {
     document.getElementById("closed").innerHTML = "We are currently closed for the season, come back between June 5th and August 20th!";
-    document.getElementById("closed").style.color='red';
+    document.getElementById("closed").style.color='maroon';
 } else if (month === 8 && day > 20) {
     document.getElementById("closed").innerHTML = "We are currently closed for the season, come back between June 5th and August 20th!";
-    document.getElementById("closed").style.color='red';
+    document.getElementById("closed").style.color='maroon';
 }
 else {
     document.getElementById("closed").innerhtml = "We are currently open for the season, come by for a snowcone during our open hours!";
